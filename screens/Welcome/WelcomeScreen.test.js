0ed2ec617f995e07e441c20b1e6e2f5e9d11698c
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-09-21 23:43:35 +0545

    Moved the render component above its scope so that every assertion can have access to it

--- screens/Welcome/WelcomeScreen.test.js
+++ screens/Welcome/WelcomeScreen.test.js
@@ -3,8 +3,8 @@ import {render} from '@testing-library/react-native';
 import WelcomeScreen from './WelcomeScreen';
 
 describe('WelcomeScreen', () => {
+  const {getByText, getByTestId, debug} = render(<WelcomeScreen />);
   it('shows the title message', () => {
-    const {getByText, getByTestId, debug} = render(<WelcomeScreen />);
     const title = getByTestId('title');
     expect(title).toHaveTextContent('Welcome to SahasJiban');
   });
